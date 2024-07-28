3b714d9a5a649c439b1b696e4d92ac4a90cd1b1b
khala1391 <khala1391@gmail.com>
2024-07-28 21:12:21 +0700

    initial commit

---
+++ data/.ipynb_checkpoints/myscript-checkpoint.py
@@ -0,0 +1,47 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8061eddc-0faa-4b20-8dbb-bd58977f9b52",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def square(x):\n",
+    "\"\"\"square a number\"\"\"\n",
+    "    return x ** 2\n",
+    "for N in range(1, 4):\n",
+    "    print(f\"{N} squared is {square(N)}\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0c1b6a51-9551-4a57-a666-7ee420c2849f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
